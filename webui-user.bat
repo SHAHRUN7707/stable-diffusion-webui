82951b05b4b141061cbe4ffa5b88b2842bbc7b82
M. SHAHRUN AZIM <145690939+SHAHRUN7707@users.noreply.github.com>
2023-12-27 11:32:49 +0600

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=listen --lowvram --precision full --no-half --opt-sub-quad-attention --opt-split-attention-v1 --disable-nan-check
+set COMMANDLINE_ARGS= --skip-torch-cuda-test
 
 call webui.bat
